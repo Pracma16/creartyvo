2ac7a4b6f9408b4cb7f2f754e5743bcc7fb7b4fd
Pracma16 <oportunilan@gmail.com>
2024-06-04 17:51:10 -0300

    home inicial ok

---
+++ Js/style.js
@@ -0,0 +1,19 @@
+
+// Texto variante en la portada
+const texts = [
+    "Hoy es tu momento, escala al siguiente nivel",
+    "¿Buscabas un cambio para evolución con la IA?",
+    "¿Buscas crecer con estrategias digitales efectivas?",
+    "Lleva tu visión al siguiente nivel.",
+    "Destacamos tu negocio en el mercado.",
+];
+
+let currentIndex = 0;
+
+function changeText() {
+    const textContainer = document.getElementById('text-container');
+    currentIndex = (currentIndex + 1) % texts.length;
+    textContainer.textContent = texts[currentIndex];
+}
+
+setInterval(changeText, 8000); // Cambia el texto cada 8 segundos
